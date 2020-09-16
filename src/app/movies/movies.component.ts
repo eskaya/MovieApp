6862ffc4b0e245f17b2bd7e99f063c22e92eef09
eskaya <esra.kaya@bil.omu.edu.tr>
2020-09-16 23:49:05 +0300

    Lesson:1 add component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'movies' , // etiket şeklinde app.component.html de çağırarak çalıştırırız
+    templateUrl: 'movies.component.html'
+})
+
+export class MoviesComponent{
+
+}
