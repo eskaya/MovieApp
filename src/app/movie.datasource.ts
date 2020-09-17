8b45adb6a3f33b5c3fa50e1fbc8362be65a7ca0e
eskaya <esra.kaya@bil.omu.edu.tr>
2020-09-17 21:08:31 +0300

    lesson:longg

---
+++ src/app/movie.datasource.ts
@@ -0,0 +1,10 @@
+import {Movie} from './movie';
+
+export const Movies: Movie[]=[
+    {id:1, name:'movie1'},
+    {id:2, name:'movie2'},
+    {id:3, name:'movie3'},
+    {id:4, name:'movie4'},
+    {id:5, name:'movie5'},
+]
+
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,22 @@
+import { style } from '@angular/animations';
+import { StylesCompileDependency } from '@angular/compiler';
 import {Component} from '@angular/core';
+import {Movies} from '../movie.datasource';
+import {Movie} from '../movie';
 
 @Component({
     selector: 'movies' , // etiket şeklinde app.component.html de çağırarak çalıştırırız
-    templateUrl: 'movies.component.html'
+    templateUrl : 'movies.component.html'
 })
 
 export class MoviesComponent{
+    title = "Movies List";
+    movies = Movies;
+    selectedMovie : Movie;
 
+
+    onSelect(movie:Movie): void{
+        this.selectedMovie =movie;
+    }
+  
 }
